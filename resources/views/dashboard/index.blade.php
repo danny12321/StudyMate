6cff5b8f86623d3acc957a2e7b6fa8ce6442fe65
Danny <d.berkelaar1@student.avans.nl>
2020-03-21 15:46:41 +0100

    QR Code

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.dashboard')
 
 @section('content')
+    <h1>Zelf kijken? Open hier de pagina</h1>
+    <img src="https://api.qrserver.com/v1/create-qr-code/?data={{ url('/') }}&size=220x220&margin=10" alt="qrcode">
+
     <h1>Voortgangsmeter</h1>
     <progress max="100" value="50"></progress>
     <meter max="100" value="50"></meter>
