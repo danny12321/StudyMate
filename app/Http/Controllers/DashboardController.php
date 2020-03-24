7e22ae84a36cf885667c8e6a9ac66c39fd6d36e9
Thierry Rietveld <thierry.rietveld0505@gmail.com>
2020-03-24 15:41:52 +0100

    Dashboard periods init

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Course;
 
 class DashboardController extends Controller
 {
     //
 
     public function index() {
-        return view('dashboard.index');
+        return view('dashboard.index', [
+            'courses' => Course::get()
+        ]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,17 +1,51 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1>Zelf kijken? Open hier de pagina</h1>
-    <img src="https://api.qrserver.com/v1/create-qr-code/?data={{ url('/') }}&size=220x220&margin=10" alt="qrcode">
+    <div class="container">
+        <h1>Zelf kijken? Open hier de pagina</h1>
+        {{-- <img src="https://api.qrserver.com/v1/create-qr-code/?data={{ url('/') }}&size=220x220&margin=10" alt="qrcode"> --}}
 
-    <h1>Voortgangsmeter</h1>
-    <progress max="100" value="50"></progress>
-    <meter max="100" value="50"></meter>
+        <h1>Voortgangsmeter</h1>
 
-    <h1>Voltooide vakken</h1>
-    <h2>Periode 1</h2>
-    <h3>Blok 1</h3>
-    <h3>Blok 2</h3>
-    <h3>Blok 3</h3>
-    <h3>Blok 4</h3>
+        <h1>Voltooide vakken</h1>
+        <h2>Periode 1</h2>
+        <h3>Blok 1</h3>
+        <h3>Blok 2</h3>
+        <h3>Blok 3</h3>
+        <h3>Blok 4</h3>
+        
+        <div class="progress">
+            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+        </div>
+
+        <?php
+            $maxBlock = 0;
+            foreach ($courses as $course) {
+                if($course->block > $maxBlock) $maxBlock = $course->block;
+            }
+        ?>
+
+        <div class="m-progress">
+
+            <div class="m-progress__block">
+                <div class="m-progress__block__content">blok</div>
+                <div class="m-progress__block__period">Periode</div>
+            </div>
+            @for ($block = $maxBlock; $block > 0; $block--)
+                {{-- Clac the period --}}
+                @if ((($block - 1) % 4) + 1 == 4)
+                    <div class="m-progress__year">
+                @endif
+
+                    <div class="m-progress__year__block">
+                        <div class="m-progress__year__block__content">{{$block}}</div>
+                        <div class="m-progress__year__block__period">{{(($block - 1) % 4) + 1}}</div>
+                    </div>
+
+                @if ((($block - 1) % 4) + 1 == 1)
+                    </div>
+                @endif
+            @endfor
+        </div>
+    </div>
 @endsection
