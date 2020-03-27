0b52e94f85e27169fbc48d1bd9443e7c32784d9b
Danny <d.berkelaar1@student.avans.nl>
2020-03-27 15:41:45 +0100

    Unit test

---
+++ tests/Unit/TeacherTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Teacher;
+use PHPUnit\Framework\TestCase;
+
+class TeacherTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_Teacher_InputName_IsEqual_OutputName()
+    {
+        // arrange
+        $teacher = new Teacher();
+        $name = 'Harry';
+
+        // act
+        $teacher->name = $name;
+
+        // assert
+        $this->assertEquals($name, $teacher->name);
+    }
+}
